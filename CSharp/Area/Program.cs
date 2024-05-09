262d48d69ca723f3507e3d33a0ef9f07e882b67b
RubensAugustoMM <rubensammiranda@gmail.com>
2024-05-09 15:52:17 -0300

    Exercício Area

---
+++ CSharp/Area/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double PI = 3.14159;
+
+        double resultado;
+        double[] numeros;
+
+        string[] entrada;
+
+        entrada = (Console.ReadLine()).Split(' ');
+
+        numeros = Array.ConvertAll(entrada, double.Parse); //0 =a, 1 = b, 2 = c
+
+        resultado = (double) numeros[0] * numeros[2] / 2;
+        Console.WriteLine(String.Format("TRIANGULO: {0:0.000}", resultado));
+
+        resultado = (double)PI * numeros[2] * numeros[2];
+        Console.WriteLine(String.Format("CIRCULO: {0:0.000}", resultado));
+
+        resultado = (double)(numeros[0] + numeros[1]) * numeros[2] / 2;
+        Console.WriteLine(String.Format("TRAPEZIO: {0:0.000}", resultado));
+
+        resultado = numeros[1] * numeros[1];
+        Console.WriteLine(String.Format("QUADRADO: {0:0.000}", resultado));
+
+        resultado = numeros[0] * numeros[1];
+        Console.WriteLine(String.Format("RETANGULO: {0:0.000}", resultado));
+
+    }
+
+}

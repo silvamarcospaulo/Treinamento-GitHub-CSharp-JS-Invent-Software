5e3484a8457e70e6d30cd4ac61fc2d3fd2892ce7
RubensAugustoMM <rubensammiranda@gmail.com>
2024-05-09 16:52:19 -0300

    exercício ArrayHash

---
+++ CSharp/ArrayHash/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string strEntrada = Console.ReadLine();
+
+        Dictionary<char, int> Alfabeto = CriaAlfabeto();
+
+        int N = int.Parse(strEntrada);
+        int L, i, soma = 0;
+
+
+
+        for(;N > 0; N--)
+        {
+            strEntrada = Console.ReadLine();
+            L = int.Parse(strEntrada);
+            
+            for(int j = 0; j < L; j++)
+            {
+                i = 0;
+                strEntrada = Console.ReadLine();
+                foreach(char c in strEntrada)
+                {
+                    soma += Alfabeto[c] + i + j;
+                    i++; 
+                }
+            }
+
+            Console.WriteLine(soma);
+            soma = 0;
+        }
+    }
+
+    public static Dictionary<char,int> CriaAlfabeto()
+    {
+        Dictionary<char, int> Alfabeto = new Dictionary<char, int>();
+
+        for(int i = 0; i < 26;i++)
+            Alfabeto.Add((char)('A'+i), i);
+
+        return Alfabeto;
+    }
+ }

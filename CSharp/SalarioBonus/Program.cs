e8f67ebc12ed0c0ce2168d8f9f344c87c8b66bb1
RubensAugustoMM <rubensammiranda@gmail.com>
2024-05-09 15:49:51 -0300

    Exercício salario com bonus

---
+++ CSharp/SalarioBonus/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string nome;
+        double salario, totalVendas;
+
+        nome = Console.ReadLine();
+        salario = double.Parse(Console.ReadLine());
+        totalVendas = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(string.Format("TOTAL = R$ {0:0.00}", salario + totalVendas*0.15));
+        
+    }
+
+}

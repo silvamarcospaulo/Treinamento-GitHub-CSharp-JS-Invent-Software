280846fcfd98d07d7a623ce692c561d41b2d3b24
RubensAugustoMM <rubensammiranda@gmail.com>
2024-05-09 16:03:40 -0300

    exercício Tell me the frequencies

---
+++ CSharp/TellTheFrequencies/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string stringEntrada;
+
+        stringEntrada = Console.ReadLine();
+        
+        while(stringEntrada != null)
+        {
+            char[] arrayChar = stringEntrada.ToCharArray();
+            char[] arrayDistinto = arrayChar.GroupBy(c => c)
+                                            .OrderBy(g => g.Count())
+                                            .ThenBy(g => g.Key)
+                                            .SelectMany(g => g)
+                                            .Distinct()
+                                            .ToArray(); 
+
+            foreach(char c in arrayDistinto)
+            {
+                int total = (from d in arrayChar
+                             where d == c
+                             select d).Count();
+                Console.WriteLine((int)c + " " + total);
+            }
+            Console.WriteLine();
+         
+            stringEntrada = Console.ReadLine();  
+        }
+        
+       
+    }
+}

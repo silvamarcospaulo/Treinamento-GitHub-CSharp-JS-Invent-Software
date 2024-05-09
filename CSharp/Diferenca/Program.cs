0c90fec019ce247886be3dcb787068397ff8394a
Marcos Paulo Silva <smpmarcospaulo@icloud.com>
2024-05-09 12:33:06 -0300

    Primeiro Commit

---
+++ CSharp/Diferenca/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int[] input = new int[4];
+        for (int i = 0; i < 4; i++)
+        {
+            input[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine($"DIFERENCA = {input[0] * input[1] - input[2] * input[3]}");
+    }
+}

39f69e5bca75234baf1a8bf748dbca685d06c711
Gabriel Detofol <detofol29@gmail.com>
2024-05-10 09:50:41 -0300

    versao parcial

--- CSharp/JogandoCartasFora/Program.cs
+++ CSharp/JogandoCartasFora/Program.cs
@@ -1,17 +1,40 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 class URI
 {
 
     static void Main(string[] args)
     {
+        List<int> cartas = new List<int>();
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        for (int i = 0; i < 50; i++)
+        {
+            Console.WriteLine("Digite o valor: ");
+            int valor = int.Parse(Console.ReadLine());
+            if (valor == 0)
+            {
+                i = 50;
+
+            }
+            else { cartas.Add(valor); }
+
+
+        }
+        int n = cartas.Count;
+        List<int> cartasDescartadas = new List<int>();
+        int[] cartasArray = cartas.ToArray();
+
+        for (int i = 0; i < cartasArray.Length; i++)
+        {
+            Console.WriteLine($"Posição{i} - {cartasArray[i]}");
+        }
+
+
+
 
     }
+
+
 
 }
